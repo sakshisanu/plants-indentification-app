083c19093329cba61285c2993819ec09bec90716
sakshisanu <sakshisanu1703@gmail.com>
2026-01-15 19:04:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,48 @@
-import streamlit as st
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
-from PIL import Image, ImageOps
-import tensorflow as tf
-@st.cache_resource
-def load_model():
-    return tf.keras.models.load_model("keras_model.h5")
 
-model = load_model()
-with open("labels.txt", "r") as f:
-    class_names = [line.strip() for line in f.readlines()]
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
-st.title("🌱 Plant Identification App")
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
 
-uploaded_file = st.file_uploader(
-    "Upload a plant image",
-    type=["jpg", "jpeg", "png"]
-)
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
-if uploaded_file is not None:
-    # 1. Open image
-    image = Image.open(uploaded_file).convert("RGB")
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    # 2. Resize image
-    image = ImageOps.fit(image, (224, 224))
+# Replace this with the path to your image
+image = Image.open("<IMAGE_PATH>").convert("RGB")
 
-    # 3. Show image
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+# resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-    # 4. Convert to array
-    image_array = np.asarray(image)
-    image_array = image_array / 255.0
-    image_array = np.expand_dims(image_array, axis=0)
+# turn the image into a numpy array
+image_array = np.asarray(image)
 
-    predictions = model.predict(image_array)
-predicted_index = np.argmax(predictions)
-confidence = predictions[0][predicted_index]
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-st.subheader("Prediction")
-st.success("🌱 Plant identified successfully!")
-st.write(f"Plant name: {class_names[predicted_index]}")
-st.write(f"Confidence: {float(confidence) * 100:.2f}%")
+# Load the image into the array
+data[0] = normalized_image_array
+
+# Predicts the model
+prediction = model.predict(data)
+index = np.argmax(prediction)
+class_name = class_names[index]
+confidence_score = prediction[0][index]
+
+# Print prediction and confidence score
+print("Class:", class_name[2:], end="")
+print("Confidence Score:", confidence_score)
+
+
 
 
 
