11004dee48675ea425a786bc4f67da0536ad0b79
sakshisanu <sakshisanu1703@gmail.com>
2026-01-15 17:34:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import numpy as np
+import tensorflow as tf
 from PIL import Image, ImageOps
 
 st.title("🌱 Plant Identification App")
+model=tf.keras.models.load_model("keras_model.h5")
+with open("labels.txt","r") as f:class_names = f.read().splitlines()
 
 uploaded_file = st.file_uploader(
     "Upload a plant image",
@@ -16,6 +20,7 @@ if uploaded_file is not None:
 
     st.image(image, caption="Uploaded Image", use_column_width=True)
    
+
 
 
 
