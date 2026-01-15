4e9075dfcb729552306f14b6fa09547320366c06
sakshisanu <sakshisanu1703@gmail.com>
2026-01-15 17:21:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
 
     size = (224, 224)
-    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
    
 
 
  st.image(image, caption="Uploaded Image", use_column_width=True)
    
+
